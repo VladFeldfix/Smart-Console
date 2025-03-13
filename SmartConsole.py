15d7ec644b71bc1e98c22518c4197adba5867f01
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2025-03-13 13:17:51 +0200

    Add files via upload

--- SmartConsole.py
+++ SmartConsole.py
@@ -135,7 +135,7 @@ class SmartConsole:
             self.fatal_error("Missing file settings.txt")
         else:
             self.__loaded_settings = {}
-            file = open("settings.txt", encoding="utf-8")
+            file = open("settings.txt")
             lines = file.readlines()
             file.close()
             for line in lines:
